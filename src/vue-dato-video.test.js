20bd05e08f4ebfc557f4b16a4e4ac99bcb8855ec
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-17 13:47:56 +0200

    update tests

--- src/vue-dato-video.test.js
+++ src/vue-dato-video.test.js
@@ -3,6 +3,22 @@ import { shallowMount } from '@vue/test-utils'
 import VueDatoVideo from './vue-dato-video.vue'
 
 test('Renders', () => {
-  const wrapper = shallowMount(VueDatoVideo)
+  const wrapper = shallowMount(VueDatoVideo, {
+    propsData: {
+      loop: false,
+      autoplay: false,
+      mute: false,
+      video: {
+        url: 'https://www.youtube.com/watch?v=AoIq7P6DJKU',
+        title: 'A Love Letter to Winter',
+        width: 480,
+        height: 270,
+        provider: 'youtube',
+        providerUid: 'AoIq7P6DJKU',
+        thumbnailUrl: 'https://i.ytimg.com/vi/AoIq7P6DJKU/hqdefault.jpg'
+      },
+    },
+  })
+
   expect(wrapper.exists()).toBe(true)
 })
