c001a037820e66e1d5e68ba55722edae76e5af35
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-17 14:14:13 +0200

    update nav in docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,14 @@
 const pkg = require('../../package.json')
 
 module.exports = {
-    title: `${pkg.name}`,
+    title: `${pkg.name} (${pkg.version})`,
     description: pkg.description,
     dest: `www`,
     themeConfig: {
         search: false,
         nav: [
-            { text: 'v1', link: '/' }
+            { text: 'v1', link: '/' },
+            { text: 'Code', link: 'https://github.com/voorhoede/vue-dato-video' }
         ]
     },
     plugins: [
